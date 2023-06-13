2363c098ec229fa0057da8dbc769231e4390c0d2
MBalinsky <supermike820@gmail.com>
2023-06-13 13:56:40 +0300

    feat: add features and new header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,24 @@
 const burger = document.querySelector('.header__icon');
-const menuParts = document.querySelectorAll('.menu-part')
+const menuParts = document.querySelectorAll('.menu-part');
+const btnClose = document.querySelector('.header__btn-close')
 
 burger.addEventListener('click', openBurger);
+btnClose.addEventListener('click', closeBurger);
 
-function openBurger(){
+function openBurger() {
     menuParts.forEach(el => {
-        el.classList.toggle('open');
+        el.classList.add('open');
     })
+    setTimeout(() => { 
+        btnClose.classList.add('header__btn-close--active')
+     }, 1000);
+}
+
+function closeBurger(){
+    menuParts.forEach(el => {
+        el.classList.remove('open');
+    })
+    btnClose.classList.remove('header__btn-close--active')
 }
 
 //* * * * * * * * * * * * * * * * * * * * * * * * * *//
